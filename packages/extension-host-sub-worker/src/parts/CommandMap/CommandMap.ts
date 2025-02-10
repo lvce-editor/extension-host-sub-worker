bed11650d6c363fae23258a310cdddf0aa40ef90
Le Vivilet <levivlet@pm.me>
2025-02-10 22:44:59 +0100

    lint

--- packages/extension-host-sub-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/extension-host-sub-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,6 +1,6 @@
 import * as LoadFile from '../LoadFile/LoadFile.ts'
-import * as WebView from '../WebView/WebView.ts'
 import * as SetPort from '../SetPort/SetPort.ts'
+import * as WebView from '../WebView/WebView.ts'
 
 export const commandMap = {
   'LoadFile.loadFile': LoadFile.loadFile,
